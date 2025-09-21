085e12cd81f1f3b14841e9caadbf9749a0975135
kaveeshala <it22110916@my.sliit.lk>
2025-09-21 16:13:16 +0530

    Create SignUp page

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
+import SignUp from "./pages/SignUp";
+
 
 export default function App() {
   return (
@@ -8,6 +10,7 @@ export default function App() {
     
         <Route path="/" element={<Navigate to="/login" replace />} />
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignUp />} />
       </Routes>
     </Router>
   );
