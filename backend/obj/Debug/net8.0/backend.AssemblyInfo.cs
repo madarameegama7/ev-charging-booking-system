88818f0bce2075292301abbfcd0e02f51eb9f7c8
Madara Meegama <madarameegama7@gmail.com>
2025-09-26 13:52:07 +0530

    Allowed access for admin and operator

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb9ff836991e92afe7f64439f432302e00343821")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efd80bc298ca27a00a302225d51b11aa509c0bce")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
