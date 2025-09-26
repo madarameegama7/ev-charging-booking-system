2c48e2b0263264dc6c39b8599fe38dace1dc31db
Madara Meegama <madarameegama7@gmail.com>
2025-09-26 11:31:05 +0530

    Changes to header comment

--- backend/Repositories/IBookingRepository.cs
+++ backend/Repositories/IBookingRepository.cs
@@ -1,12 +1,5 @@
-// ------------------------------------------------------------
-// Sri Lanka Institute of Information Technology
-// Module: SE4040 – Enterprise Application Development
-// Project: EV Charging Station Booking System
 // File: IBookingRepository.cs
-// Author: <Your Name / IT Number>
-// Description: Repository interface for bookings.
-// Date: 2025-09-25
-// ------------------------------------------------------------
+// Description: Repository interface for bookings
 using Backend.Models;
 
 namespace Backend.Repositories
