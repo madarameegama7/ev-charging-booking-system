4f1a9068b0042d1f5d60be0c68d94eb9708b31c6
Janudi <januditharushika@gmail.com>
2025-10-09 13:21:45 +0530

    Update the Splash Screen

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a4d927644af57bbf52f694b9c2079ce9dce0ea2d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7eb75a7950c11887b1206c55930a0453a41a9aa1")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
