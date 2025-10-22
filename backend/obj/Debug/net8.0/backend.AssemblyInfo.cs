c9b644bee32af32623e27ca28a31ccb8245ac8e8
Madara Meegama <madarameegama7@gmail.com>
2025-10-22 19:43:17 +0530

    Mobile app setup

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b56439ebe615e564239c243df8f782e2d6af7ebb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dacc907ad04fc1af5059f546edaf3b7fbc77997c")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
